3906545da986c314bd4ebd23062703b047a07d3e
Giannis Halvatzakis <ghalvatzakis2@gmail.com>
2014-07-04 10:55:57 +0300

    added content boxes and dynamic background

--- js/script.js
+++ js/script.js
@@ -6,5 +6,16 @@
 $('html').hide();
 $(document).ready(function() {
     $('html').fadeIn("slow");
+//    $('body').css("background-image", "url(1.jpg)");
+//    //dynamic background changing
+//    setInterval(function() {
+//        changeBackground();
+//    }, 5000);
+//    function changeBackground() {
+//        var i = Math.random() * (5 - 1) + 1;
+//        i = Math.floor(i);
+//        $('body').css("background-image", "url(" + i + ".jpg)");
+//
+//    }
 });
 
