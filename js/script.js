d854a99bdf4fca6b96629791deba2aed7e635553
Giannis Halvatzakis <ghalvatzakis2@gmail.com>
2014-07-19 18:33:09 +0300

    added resizable menu on scroll

--- js/script.js
+++ js/script.js
@@ -19,3 +19,19 @@ $(document).ready(function() {
 //    }
 });
 
+//SCROLL RESIZE MENU
+function init() {
+    window.addEventListener('scroll', function(e){
+        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+            shrinkOn = 300,
+            header = document.querySelector("#header");
+        if (distanceY > shrinkOn) {
+            classie.add(header,"smaller");
+        } else {
+            if (classie.has(header,"smaller")) {
+                classie.remove(header,"smaller");
+            }
+        }
+    });
+}
+window.onload = init();
